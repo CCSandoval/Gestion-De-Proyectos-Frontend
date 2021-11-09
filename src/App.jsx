88e33dc43529aada9dfc29bbcbb5569878ec1ac9
Cristian <krcamilo0417@gmail.com>
2021-11-08 23:59:17 -0500

    Sidebar completo, responsive removido porqué me confundía

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import PrivateLayout from 'layouts/PrivateLayout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { UserContext } from 'context/userContext';
-import Index from 'pages/Index';
-import Page2 from 'pages/Page2';
-import IndexCategory1 from 'pages/category1/Index';
-import Category1 from 'pages/category1/CategoryPage1';
-import 'styles/globals.css';
-
+import React, { useState } from "react";
+import PrivateLayout from "layouts/PrivateLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { UserContext } from "context/userContext";
+import "styles/globals.css";
+import Index from "pages/Index";
+import Usuarios from "pages/Usuarios";
+import MisProyectos from "pages/MisProyectos";
+import Auth from "pages/Auth";
 // import PrivateRoute from 'components/PrivateRoute';
 
 function App() {
@@ -16,19 +15,19 @@ function App() {
 
   return (
     <Auth0Provider
-      domain='misiontic-concesionario.us.auth0.com'
-      clientId='WsdhjjQzDLIZEHA6ouuxXGxFONFGAQ4g'
-      redirectUri='http://localhost:3000/admin'
-      audience='api-autenticacion-concesionario-mintic'
+      domain="misiontic-concesionario.us.auth0.com"
+      clientId="WsdhjjQzDLIZEHA6ouuxXGxFONFGAQ4g"
+      redirectUri="http://localhost:3000/admin"
+      audience="api-autenticacion-concesionario-mintic"
     >
       <UserContext.Provider value={{ userData, setUserData }}>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<PrivateLayout />}>
-              <Route path='' element={<Index />} />
-              <Route path='page2' element={<Page2 />} />
-              <Route path='category1' element={<IndexCategory1 />} />
-              <Route path='category1/page1' element={<Category1 />} />
+            <Route path="/" element={<PrivateLayout />}>
+              <Route path="" element={<Index />} />
+              <Route path="usuarios" element={<Usuarios />} />
+              <Route path="/mis-proyectos" element={<MisProyectos />} />
+              <Route path="/auth" element={<Auth />} />
             </Route>
           </Routes>
         </BrowserRouter>
---
+++ src/pages/MisProyectos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MisProyectos = () => {
+    return (
+        <div>
+        <div className="bg-gray-300 h-96">MIS PROYECTOS</div>
+        <div className="bg-gray-300 h-96">MIS PROYECTOS</div>
+        <div className="bg-gray-300 h-96">MIS PROYECTOS</div>
+        <div className="bg-gray-300 h-96">MIS PROYECTOS</div>
+      </div>
+    )
+}
+
+export default MisProyectos
