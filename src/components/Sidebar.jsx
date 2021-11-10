e6d27967c135f1e014a5f272e5b14f1beaaa5477
alex <castro.t.alex@gmail.com>
2021-11-10 11:54:27 -0500

    creacion de dialogo del nombre del usuario

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
+import { Dialog } from "@material-ui/core";
+
 
 const SidebarButtons = () => {
   return (
@@ -24,12 +26,16 @@ const SidebarButtons = () => {
 };
 
 const Logo = () => {
+  const [newProfile, setNewProfile]= useState(false);
   return (
-    <button
+    
+   
+    <> <button
       type="button"
       onClick={(e) => {
-        e.preventDefault();
-        alert("dialogo de perfil");
+        e.preventDefault()
+        console.log("si le damos click")
+        setNewProfile(!newProfile)
       }}
       className="py-2 px-1 w-full flex items-center justify-center bg-purple-800 hover:bg-purple-900 text-left"
     >
@@ -45,7 +51,54 @@ const Logo = () => {
           Rol
         </span>
       </div>
-    </button>
+      
+    </button> 
+    <Dialog open={newProfile}>
+    <h2 className="text-center text-2xl mt-3">Mi perfil</h2>
+    <div className="w-full max-w-lg">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                  Nombre del usuario
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" />
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                  Correo del usuario
+                </label>
+                <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" />
+
+              </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                  N- Identificacón
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" />
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                  Contraseña
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="password" />
+              </div>
+          </div>
+        </form>
+        <div className="flex justify-evenly">
+            <i className="bi bi-check-circle-fill text-green-500 text-4xl cursor-pointer mx-auto my-4"></i>
+            <i className="bi bi-x-circle-fill text-red-500 text-4xl cursor-pointer mx-auto my-4" 
+            onClick={()=>{setNewProfile(!newProfile)
+              }}
+            ></i>
+        </div>
+        </div>
+    
+    </Dialog>
+</>
+    
   );
 };
 
