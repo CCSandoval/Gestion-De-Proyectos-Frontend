b8bd4d4a7d304b63f5c588c56992f9771e86ce4b
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-11-13 21:26:35 -0500

    agregar botón para agregar avances, botones para editar avances, confirmar y cancelar

--- src/components/CardAvances.jsx
+++ src/components/CardAvances.jsx
@@ -1,13 +1,13 @@
-import { Dialog } from "@material-ui/core";
+import { Dialog, Tooltip } from "@material-ui/core";
 import React from "react";
 import { useState } from "react";
 
 const CardAvances = ({ id, fecha, ob, icono }) => {
-
   const [checkInfo, setCheckInfo] = useState(false);
+  const [isediting, setIsediting] = useState(false);
 
-  const Icono = ({ icono }) => {
-    if (icono === "info-circle") {
+  const Icono = ({ icon }) => {
+    if (icon === "info-circle") {
       return (
         <button
           onClick={() => {
@@ -23,7 +23,7 @@ const CardAvances = ({ id, fecha, ob, icono }) => {
       return (
         <button className="flex flex-col justify-center items-center">
           <span className="text-xs">Añadir Observación</span>
-          <i className={`bi bi-${icono} w-1/2 text-5xl`}></i>
+          <i className={`bi bi-${icon} w-1/2 text-5xl`}></i>
         </button>
       );
     }
@@ -36,29 +36,73 @@ const CardAvances = ({ id, fecha, ob, icono }) => {
         <span>{`Fecha ${fecha}`}</span>
       </div>
       <div className="flex w-full justify-around">
-        <div className="flex w-3/4 m-1 p-1">
-          <span>Aportes del Avance: </span>
-          <p className="w-full">
-            Luctus phasellus tincidunt eleifend porta parturient non tortor
-            dictumst, aliquet vestibulum himenaeos pulvinar ullamcorper euismod
-            mollis vitae platea, conubia posuere cubilia feugiat habitant
-            blandit pretium.
-          </p>
-        </div>
+        {isediting ? (
+          <div className="flex w-3/4 m-1 p-1">
+            <div className="flex flex-col">
+              <div className="flex flex-row">
+                <Tooltip title="Confirmar edición" arrow>
+                  <i
+                    className="bi bi-check-circle-fill cursor-pointer m-1 text-xl text-green-500 font-bold hover:text-green-700 shadow-md"
+                    onClick={() => {
+                      setIsediting(false);
+                    }}
+                  ></i>
+                </Tooltip>
+                <Tooltip title="Cancelar edición" arrow>
+                  <i
+                    className="bi bi-x-circle-fill cursor-pointer m-1 text-xl text-red-500 font-bold hover:text-red-700 shadow-md"
+                    onClick={() => {
+                      setIsediting(false);
+                    }}
+                  ></i>
+                </Tooltip>
+              </div>
+              <span>Aportes del Avance: </span>
+            </div>
+            <textarea
+              type="text"
+              className="w-full p-1 m-1 overflow-y-scroll rounded-md bg-blue-50 focus:outline-none focus:ring focus:ring-blue-300"
+            ></textarea>
+          </div>
+        ) : (
+          <div className="flex w-3/4 m-1 p-1">
+            <div className="flex flex-col">
+              <div className="flex flex-row">
+                <Tooltip title="Editar avance" arrow>
+                  <i
+                    className="w-auto bi bi-pencil-fill cursor-pointer font-bold text-blue-500 hover:text-blue-700 shadow-md"
+                    onClick={() => {
+                      setIsediting(true);
+                    }}
+                  ></i>
+                </Tooltip>
+              </div>
+              <span>Aportes del Avance: </span>
+            </div>
+            <p className="w-full">
+              Luctus phasellus tincidunt eleifend porta parturient non tortor
+              dictumst, aliquet vestibulum himenaeos pulvinar ullamcorper
+              euismod mollis vitae platea, conubia posuere cubilia feugiat
+              habitant blandit pretium.
+            </p>
+          </div>
+        )}
         <div className="flex flex-col justify-center items-center cursor-pointer">
-          <Icono icono={icono} />
+          <Icono icon={icono} />
         </div>
       </div>
       <Dialog open={checkInfo}>
-        <div className='flex flex-col bg-blue-50'>
+        <div className="flex flex-col bg-blue-50">
           <i
             onClick={() => {
               setCheckInfo(false);
             }}
             className="flex justify-end bi bi-x-circle cursor-pointer text-2xl p-1 m-1"
           ></i>
-          <h2 className='flex justify-center p-1 m-2 font-bold'>OBSERVACIONES</h2>
-          <ul className='p-3 m-3 border-2 border-gray-300 rounded-md'>
+          <h2 className="flex justify-center p-1 m-2 font-bold">
+            OBSERVACIONES
+          </h2>
+          <ul className="p-3 m-3 border-2 border-gray-300 rounded-md">
             <li>Escriba aquí las observaciones</li>
             <li>Escriba aquí las observaciones</li>
           </ul>
--- src/pages/Avances.jsx
+++ src/pages/Avances.jsx
@@ -1,22 +1,34 @@
 import CardAvances from "components/CardAvances";
 import CardInvestigadores from "components/CardInvestigadores";
+import { nanoid } from "nanoid";
 import React from "react";
 
 const Avances = () => {
   return (
     <>
-      <div className="flex w-full justify-center text-3xl bg-gray-200 p-2">NOMBRE DEL PROYECTO</div>
+      <div className="flex w-full justify-center text-3xl bg-gray-200 p-2">
+        NOMBRE DEL PROYECTO
+      </div>
       <div className="flex w-full justify-around">
         <div className="flex flex-col p-5 w-4/5 h-screen bg-gray-200 border-t-2 border-gray-600">
-          <div className="flex w-full justify-center p-1 text-xl">AVANCES</div>
+          <div className="flex w-full justify-center p-1 text-xl m-5">
+            AVANCES
+          </div>
           <div>
-            <CardAvances icono='plus-circle' fecha='DD/MM/YYYY' id='111'/>
-            <CardAvances icono='info-circle' fecha='DD/MM/YYYY'id='123'/>
+            <div className='flex w-full justify-center m-5'>
+              <button className=" w-auto bg-green-400 p-3 rounded-full hover:bg-green-500 shadow-md">
+                AÑADIR AVANCE
+              </button>
+            </div>
+            <CardAvances icono="plus-circle" fecha="DD/MM/YYYY" id="111" />
+            <CardAvances icono="info-circle" fecha="DD/MM/YYYY" id="123" />
           </div>
         </div>
         <div className="flex flex-col items-center p-5 w-2/5 bg-gray-200 border-t-2 border-l-2 border-gray-600">
-          <div className="flex w-full justify-center p-1 text-xl">INVESTIGADORES</div>
-          <CardInvestigadores/>
+          <div className="flex w-full justify-center p-1 text-xl m-5">
+            INVESTIGADORES
+          </div>
+          <CardInvestigadores />
         </div>
       </div>
     </>
